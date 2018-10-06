719bf15cf68a9e556a2bb2441aa82c131db20a94
Lenilson DeSantis <lenilsonjunior.jr@gmail.com>
2018-10-06 13:35:43 -0300

    :wrench: Temas e projetos na home

--- linhasdotempo/home.php
+++ linhasdotempo/home.php
@@ -18,6 +18,47 @@ $peridiocidade = array_reverse(array('Dia', 'Mês', 'Ano', 'Década', 'Século')
 $lt_posts = array();
 ?>
 
+<section class="home">
+  
+  <div class="row" style="text-align: center;">
+    <div class="col-md-6">
+      <h1>Projetos</h1>
+      <div class="projects">
+        <?php
+        $args = array( 'post_type' => 'projects', 'posts_per_page' => -1 );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+        ?>
+          <div class="project-card">
+            <span class="date"><?php echo date_parse(get_post_meta(get_the_ID(), 'data_inicio', true))['year'] ?></span>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_excerpt(); ?></p>
+          </div>
+        <?php
+        endwhile;
+        ?>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <h1>Temas</h1>
+      <div class="projects">
+        <?php
+        foreach(get_categories(array('taxonomy' => 'tema')) as $theme ):
+        ?>
+          <div class="project-card">
+            <h2>
+              <a href="<?php echo get_term_link($theme->term_id, 'tema'); ?>"><?php echo $theme->name; ?></a>
+            </h2>
+          </div>
+        <?php
+        endforeach;
+        ?>
+      </div>
+    </div>
+  </div>
+
+</section>
+
 <section id="cd-timeline" class="cd-container">
 
 	<?php foreach($years as $year): $lt_posts[$year] = array(); ?>
